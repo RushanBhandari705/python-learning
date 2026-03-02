ad7eff876bf8fc63d79a63a5138299b4d71517f3
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-02 18:24:42 +0545

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,17 @@
+score = 0
+
+answer = input("What is the capital of France? ")
+if answer.lower() == "paris":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong!")
+
+answer = input("What is 5 * 6? ")
+if answer == "30":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong!")
+
+print("Your final score is:", score)

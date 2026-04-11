3cb40f8d382765189341277f6f089e98519ecdcf
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-04-11 19:00:14 +0545

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-#Write a program that asks the user to enter two number A and B and then code to swap your values of A and B and print them out.
+
 A= int(input("Enter the first number:"))
 B=int(input("Enter the second number:"))
 print("Your first number is:", A, "and Your second number is:", B)
